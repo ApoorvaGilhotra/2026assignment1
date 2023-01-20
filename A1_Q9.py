2a94db9dcc45557ea938aa22b74769160fc971dc
ApoorvaGilhotra <121493562+ApoorvaGilhotra@users.noreply.github.com>
2023-01-21 03:00:45 +0530

    Update A1_Q9.py

--- A1_Q9.py
+++ A1_Q9.py
@@ -1,1 +1,25 @@
+num1=int(input())
+num2=int(input())
+if(num1>num2):
+    smaller=num2
+else:
+    smaller=num1
+for i in range(1,smaller+1):
+    if(num1%i==0 and num2%i==0):
+        hcf=i
+    else:
+        smaller=smaller+1
+print(hcf)
+
+if(num1>num2):
+    greater=num1
+else:
+    greater=num2
+while (True):
+    if(greater%num1==0 and greater%num2==0):
+        print(greater)
+        break
+
+    else:
+        greater=greater+1
 
