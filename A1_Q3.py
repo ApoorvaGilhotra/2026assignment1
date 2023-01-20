637aaffca7ca92356e8fe112c11d6aaae42c755b
ApoorvaGilhotra <121493562+ApoorvaGilhotra@users.noreply.github.com>
2023-01-20 18:13:54 +0530

    Update A1_Q3.py

--- A1_Q3.py
+++ A1_Q3.py
@@ -1,12 +1,15 @@
-low = int(input())
-high = int(input())
-
-for n in range(low, high + 1):
-    if (low >= 2 and low < high and high < 10 ** 6):
-        if (n > 1):
-            for i in range(2, n):
-                if (n % i == 0):
-                    print(end="")
-                    break
-            else:
-                print(n)
+t=int(input())
+for i in range(0,t):
+   
+    n=int(input())
+    
+    if(n>1):
+        for j in range(2,n):
+            if(n%j==0):
+                print("not prime")
+                break
+        else:
+            print("prime")
+    elif(n==1):
+        print("not prime")
+                 
