87cd114bb913129b4447aa9b641fea3fa4e91753
ApoorvaGilhotra <121493562+ApoorvaGilhotra@users.noreply.github.com>
2023-01-21 01:25:41 +0530

    Update A1_Q8.py

--- A1_Q8.py
+++ A1_Q8.py
@@ -1,19 +1,32 @@
 n=(input())
 k=int(input())
-x=list(map(int,n))
+x=[]
+for i in range(int(n)):
+    x.append(i)
 
 if(k==0):
     print(n)
 elif(k>0):
+    y = x.copy()
+    for a in range(0,k):
 
-    for j in range(1,len(n)):
+        for j in range(1,len(n)):
 
-        x[j]=x[j-1]
-
-    for i in range(k):
-        a = int(n) % 10
-
-        x[i] = a
-
-    for k in range(x):
-        print(k,end="")
+            x[j] = y[j - 1]
+        c=len(y)-1
+        b = y[c]
+        x[0] = b
+        y=x.copy()
+    for k in range(len(x)):
+        c=x[k]
+        print(c,end="")
+        
+elif(k<0):
+    for a in range(abs(k)):
+    
+    
+        x.append(x.pop(0))
+    for k in range(len(x)):
+        c=x[k]
+        print(c,end="")
+    
